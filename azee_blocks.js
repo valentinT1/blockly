12780dff5b0d4f4a520ccb1f85af1db5438137db
Valentin.Tamone <valentin.tamone@etu.unige.ch>
2020-08-19 06:10:26 +0200

    added lots of azops added text input in words, added word "me" (sigma)

--- azee_blocks.js
+++ azee_blocks.js
@@ -19,6 +19,117 @@ Blockly.Blocks['container'] = {
   }
 };
 
+//AZOPS
+Blockly.Blocks['info-about'] = {
+  init: function() {
+
+    this.appendStatementInput('VALUE0')
+      .appendField('topic');
+    this.appendDummyInput('egal')
+      .appendField(new Blockly.FieldImage("../media/azops_images/egal.png", 20, 20, "*"))
+    this.appendStatementInput('VALUE1')
+      .appendField('info');
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);
+    this.setColour(colorAzops);
+    this.setTooltip('Returns a score');
+  },
+  onchange: function() {
+    fieldNameCheck(this);
+  }
+};
+
+Blockly.Blocks['add-side-info'] = {
+  init: function() {
+
+    this.appendStatementInput('VALUE0')
+      .appendField('topic');
+    this.appendStatementInput('VALUE1')
+      .appendField('(info)');
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);
+    this.setColour(colorAzops);
+    this.setTooltip('Returns a score');
+  },
+  onchange: function() {
+    fieldNameCheck(this);
+  }
+};
+
+Blockly.Blocks['category'] = {
+  init: function() {
+
+    this.appendStatementInput('VALUE0')
+      .appendField('category');
+    this.appendDummyInput('down_arrow')
+      .appendField(new Blockly.FieldImage("../media/azops_images/down_arrow.png", 20, 20, "*"))
+    this.appendStatementInput('VALUE1')
+      .appendField('focus');
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);
+    this.setColour(colorAzops);
+    this.setTooltip('Returns a score');
+  },
+  onchange: function() {
+    fieldNameCheck(this);
+  }
+};
+
+Blockly.Blocks['nicht-sondern'] = {
+  init: function() {
+
+    this.appendStatementInput('VALUE0')
+      .appendField('nicht');
+    this.appendStatementInput('VALUE1')
+      .appendField('sondern');
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);
+    this.setColour(colorAzops);
+    this.setTooltip('Returns a score');
+  },
+  onchange: function() {
+    fieldNameCheck(this);
+  }
+};
+
+Blockly.Blocks['context-ssp'] = {
+  init: function() {
+
+    this.appendStatementInput('VALUE0')
+      .appendField('context');
+    this.appendDummyInput('slash')
+      .appendField(new Blockly.FieldImage("../media/azops_images/slash.png", 20, 20, "*"))
+    this.appendStatementInput('VALUE1')
+      .appendField('événement');
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);
+    this.setColour(colorAzops);
+    this.setTooltip('Returns a score');
+  },
+  onchange: function() {
+    fieldNameCheck(this);
+  }
+};
+
+Blockly.Blocks['seq-res'] = {
+  init: function() {
+
+    this.appendStatementInput('VALUE0')
+      .appendField('condition');
+    this.appendDummyInput('right arrow')
+      .appendField(new Blockly.FieldImage("../media/azops_images/right_arrow.png", 20, 20, "*"))
+    this.appendStatementInput('VALUE1')
+      .appendField('résultat');
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);
+    this.setColour(colorAzops);
+    this.setTooltip('Returns a score');
+  },
+  onchange: function() {
+    fieldNameCheck(this);
+  }
+};
+
 Blockly.Blocks['each-of'] = {
   init: function() {
     var childCount = 0;
@@ -47,15 +158,130 @@ Blockly.Blocks['each-of'] = {
     fieldNameCheck(this);
   }
 };
-Blockly.Blocks['info-about'] = {
+
+Blockly.Blocks['all-of'] = {
+  init: function() {
+    var childCount = 0;
+
+    this.appendStatementInput('VALUE0')
+      .appendField('all-of');
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);
+    this.setColour(colorAzops);
+    this.setTooltip('Permet de regrouper plusieurs mots');
+
+    this.setOnChange(function(event) {
+      console.log("event type: " + event.type + " childcount: " + this.childBlocks_.length);
+      if (Blockly.Events.UI === event.type) {
+        if (this.childBlocks_.length - childCount > 0) {
+          this.appendStatementInput('VALUE' + (childCount + 1));
+        }
+        else if (this.childBlocks_.length - childCount < 0) {
+          this.removeInput('VALUE' + (childCount));
+        }
+      }
+      childCount = this.childBlocks_.length;
+    });
+  },
+  onchange: function() {
+    fieldNameCheck(this);
+  }
+};
+
+Blockly.Blocks['mutex-list'] = {
+  init: function() {
+    var childCount = 0;
+
+    this.appendStatementInput('VALUE0')
+      .appendField('mutex-list');
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);
+    this.setColour(colorAzops);
+    this.setTooltip('Permet de regrouper plusieurs mots');
+
+    this.setOnChange(function(event) {
+      console.log("event type: " + event.type + " childcount: " + this.childBlocks_.length);
+      if (Blockly.Events.UI === event.type) {
+        if (this.childBlocks_.length - childCount > 0) {
+          this.appendStatementInput('VALUE' + (childCount + 1));
+        }
+        else if (this.childBlocks_.length - childCount < 0) {
+          this.removeInput('VALUE' + (childCount));
+        }
+      }
+      childCount = this.childBlocks_.length;
+    });
+  },
+  onchange: function() {
+    fieldNameCheck(this);
+  }
+};
+
+Blockly.Blocks['open-list-non-mutex'] = {
+  init: function() {
+    var childCount = 0;
+
+    this.appendStatementInput('VALUE0')
+      .appendField('open-list-non-mutex');
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);
+    this.setColour(colorAzops);
+    this.setTooltip('Permet de regrouper plusieurs mots');
+
+    this.setOnChange(function(event) {
+      console.log("event type: " + event.type + " childcount: " + this.childBlocks_.length);
+      if (Blockly.Events.UI === event.type) {
+        if (this.childBlocks_.length - childCount > 0) {
+          this.appendStatementInput('VALUE' + (childCount + 1));
+        }
+        else if (this.childBlocks_.length - childCount < 0) {
+          this.removeInput('VALUE' + (childCount));
+        }
+      }
+      childCount = this.childBlocks_.length;
+    });
+  },
+  onchange: function() {
+    fieldNameCheck(this);
+  }
+};
+
+Blockly.Blocks['time-seq'] = {
+  init: function() {
+    var childCount = 0;
+
+    this.appendStatementInput('VALUE0')
+      .appendField('time-seq');
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);
+    this.setColour(colorAzops);
+    this.setTooltip('Permet de regrouper plusieurs mots');
+
+    this.setOnChange(function(event) {
+      console.log("event type: " + event.type + " childcount: " + this.childBlocks_.length);
+      if (Blockly.Events.UI === event.type) {
+        if (this.childBlocks_.length - childCount > 0) {
+          this.appendStatementInput('VALUE' + (childCount + 1));
+        }
+        else if (this.childBlocks_.length - childCount < 0) {
+          this.removeInput('VALUE' + (childCount));
+        }
+      }
+      childCount = this.childBlocks_.length;
+    });
+  },
+  onchange: function() {
+    fieldNameCheck(this);
+  }
+};
+
+Blockly.Blocks['inter-subjectivity'] = {
   init: function() {
 
     this.appendStatementInput('VALUE0')
-      .appendField('topic');
-    this.appendDummyInput('soleil')
-      .appendField(new Blockly.FieldImage("../media/azops_images/egal.png", 10, 10, "*"))
-    this.appendStatementInput('VALUE1')
-      .appendField('info');
+      .appendField('vrai');
+    this.appendDummyInput('right arrow')
+      .appendField(new Blockly.FieldImage("../media/azops_images/check.png", 20, 20, "*"))
     this.setNextStatement(true);
     this.setPreviousStatement(true);
     this.setColour(colorAzops);
@@ -66,36 +292,63 @@ Blockly.Blocks['info-about'] = {
   }
 };
 
-Blockly.Blocks['add-side-info'] = {
+Blockly.Blocks['cam-switch'] = {
   init: function() {
+    var childCount = 0;
 
     this.appendStatementInput('VALUE0')
-      .appendField('topic');
-    this.appendDummyInput('soleil')
-      .appendField(new Blockly.FieldImage("../media/azops_images/egal.png", 10, 10, "*"))
-    this.appendStatementInput('VALUE1')
-      .appendField('info');
+      .appendField('cam-switch');
     this.setNextStatement(true);
     this.setPreviousStatement(true);
     this.setColour(colorAzops);
-    this.setTooltip('Returns a score');
+    this.setTooltip('Permet de regrouper plusieurs mots');
+
+    this.setOnChange(function(event) {
+      console.log("event type: " + event.type + " childcount: " + this.childBlocks_.length);
+      if (Blockly.Events.UI === event.type) {
+        if (this.childBlocks_.length - childCount > 0) {
+          this.appendStatementInput('VALUE' + (childCount + 1));
+        }
+        else if (this.childBlocks_.length - childCount < 0) {
+          this.removeInput('VALUE' + (childCount));
+        }
+      }
+      childCount = this.childBlocks_.length;
+    });
   },
   onchange: function() {
     fieldNameCheck(this);
   }
 };
 
-Blockly.Blocks['nicht-sondern'] = {
+//MOTS
+Blockly.Blocks['textAzee'] = {
+  // Numeric input.
   init: function() {
+    this.appendDummyInput('mot')
+      .appendField("mot")
+      .appendField(new Blockly.FieldTextInput('\'\''),
+        'FIELDNAME');
 
-    this.appendStatementInput('VALUE0')
-      .appendField('nicht');
-    this.appendStatementInput('VALUE1')
-      .appendField('sondern');
-    this.setNextStatement(true);
     this.setPreviousStatement(true);
-    this.setColour(colorAzops);
-    this.setTooltip('Returns a score');
+    this.setNextStatement(false, 'Field');
+    this.setColour(colorMots);
+    this.setTooltip('This is a score');
+  },
+  onchange: function() {
+    fieldNameCheck(this);
+  }
+};
+
+Blockly.Blocks['sigma'] = {
+  // Numeric input.
+  init: function() {
+    this.appendDummyInput('sigma')
+      .appendField(new Blockly.FieldImage("../media/words/sigma.png", 30, 30, "*"))
+    this.setPreviousStatement(true);
+    this.setNextStatement(false, 'Field');
+    this.setColour(colorMots);
+    this.setTooltip('This is a score');
   },
   onchange: function() {
     fieldNameCheck(this);
@@ -107,7 +360,6 @@ Blockly.Blocks['soleil'] = {
   init: function() {
     this.appendDummyInput('soleil')
       .appendField(new Blockly.FieldImage("../media/words/soleil.png", 30, 30, "*"))
-    //this.setPreviousStatement(true, 'Field');
     this.setPreviousStatement(true);
     this.setNextStatement(false, 'Field');
     this.setColour(colorMots);
@@ -123,7 +375,6 @@ Blockly.Blocks['nuage'] = {
   init: function() {
     this.appendDummyInput('nuage')
       .appendField(new Blockly.FieldImage("../media/words/nuage.png", 30, 30, "*"))
-    //this.setPreviousStatement(true, 'Field');
     this.setPreviousStatement(true);
     this.setNextStatement(false, 'Field');
     this.setColour(colorMots);
@@ -139,7 +390,6 @@ Blockly.Blocks['pluie'] = {
   init: function() {
     this.appendDummyInput('pluie')
       .appendField(new Blockly.FieldImage("../media/words/pluie.png", 30, 30, "*"))
-    //this.setPreviousStatement(true, 'Field');
     this.setPreviousStatement(true);
     this.setNextStatement(false, 'Field');
     this.setColour(colorMots);
@@ -155,7 +405,6 @@ Blockly.Blocks['eclair'] = {
   init: function() {
     this.appendDummyInput('eclair')
       .appendField(new Blockly.FieldImage("../media/words/eclair.png", 30, 30, "*"))
-    //this.setPreviousStatement(true, 'Field');
     this.setPreviousStatement(true);
     this.setNextStatement(false, 'Field');
     this.setColour(colorMots);
